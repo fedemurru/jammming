99ca5f5ffbd0b6670afd07eaf0182c89ca3a6c5e
Federico Murru <djfedemurru@icloud.com>
2023-01-06 16:35:11 +0000

    Committttt01

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,12 +6,13 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
-  constructor(props){
-  super(props);
+  constructor(props) {
+    super(props);
+    
 
-  this.state = {
-SearchResults:[{name: 'name1', artist: 'artist1', album:'album1', id: 1}
- , {name: 'name2', artist: 'artist2', album:'album2', id: 2} 
+    this.state = {
+      searchResults: [{name: 'name1', artist: 'artist1', album:'album1', id: 1}
+, {name: 'name2', artist: 'artist2', album:'album2', id: 2}
 , {name: 'name3', artist: 'artist3', album:'album3', id: 3}]
 
   }
@@ -21,15 +22,17 @@ SearchResults:[{name: 'name1', artist: 'artist1', album:'album1', id: 1}
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar />
-          <div className="App-playlist">
-            <SearchResults  searchResults={this.state.searchResults} /> 
-             <Playlist />
+            <SearchBar />
+            <div className="App-playlist">
+              <SearchResults SearchResults={this.state.SearchResults} tracks={this.state.tracks}/>
+              <Playlist playlist={this.state.SearchResults} tracks={this.state.tracks}/>
+            </div>
           </div>
-        </div>
       </div>
-    );
+    )
   }
+  
 }
+
 
 export default App;
