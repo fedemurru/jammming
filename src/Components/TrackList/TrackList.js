c506b7bec0f6d5178296cf9e32551d8ffc75ec38
Federico Murru <djfedemurru@icloud.com>
2023-01-06 17:15:02 +0000

    Newcom

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -7,14 +7,16 @@ class TrackList extends React.Component {
     render() {
         return (
             <div className="TrackList">
-                {
-                    this.props.tracks.map(track => {
-                        return (
-                            <Track track={track} key={track.id} />
-                        )
-                    })
-                }
-            </div>
+            {
+              this.props.tracks.map(track => {
+                return <Track track={track}
+                              key={track.id}
+                              onAdd={this.props.onAdd}
+                              isRemoval={this.props.isRemoval}
+                              onRemove={this.props.onRemove} />
+              })
+            }
+          </div>
         )
     }
 }
