a00bcf4508c3afada2803163c8b2294c058f8e1b
Federico Murru <djfedemurru@icloud.com>
2023-01-07 08:49:36 +0000

    070123 committ1

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -16,6 +16,8 @@ class App extends React.Component {
 				{ name: "search result 1", artist: "artist2", album: "album2", id: 2 },
 				{ name: "search result 1", artist: "artist3", album: "album3", id: 3 },
 			],
+
+			playlistName: 'My Playlist',
 			//you didn't have a state created for playlistTracks
 			playlistTracks: [
 				{ name: "playlist track 1", artist: "artist1", album: "album1", id: 1 },
@@ -23,7 +25,42 @@ class App extends React.Component {
 				{ name: "playlist track 3", artist: "artist3", album: "album3", id: 3 },
 			],
 		};
+		this.addTrack = this.addTrack.bind(this);
+		this.removeTrack = this.removeTrack.bind(this);
+		this.updatePlaylistName = this.updatePlaylistName.bind(this);
+		this.savePlaylist = this.savePlaylist.bind(this);
+		this.search = this.search.bind(this);
 	}
+	addTrack(track) {
+		let tracks = this. state.playlistTracks;
+		if (tracks.find(savedTrack => savedTrack. id === track. id)) {
+		return 
+		}
+		tracks.push (track) ;
+		this.setState({ playlistTracks: tracks }) ;
+		}
+
+		removeTrack(track) {
+		let tracks = this.state.playlistTracks;
+		tracks = tracks. filter (currentTrack => currentTrack.id !== track.id)
+		this.setState({playlistTracks: tracks});
+		}
+
+		updatePlaylistName(name) {
+			this.setState({playlistName: name });
+
+		  }
+
+		savePlaylist() {
+			let tracksUri = this.state.playlistTracks.map(track => track.uri);
+			Spotify.savePlaylist(this.state.playlistName, tracksUri);
+		  }
+
+		async search(term) {
+			Spotify.getAccessToken();
+			let searchResults = await Spotify.search(term);
+			this.setState({searchResults: searchResults});
+		  }
 
 	render() {
 		return (
@@ -32,12 +69,13 @@ class App extends React.Component {
 					Ja<span className="highlight">mmm</span>ing
 				</h1>
 				<div className="App">
-					<SearchBar />
+					<SearchBar onSearch={this.search} />
 					<div className="App-playlist">
 						<SearchResults
 							// This wasn't working because you were passing 'SearchResults'
 							// as a prop to <SearchResults /> instead of 'searchResults'
 							searchResults={this.state.searchResults}
+							onAdd={this.addTrack}
 
 							//you don't need this
 							// tracks={this.state.tracks}
@@ -50,7 +88,12 @@ class App extends React.Component {
 
 						{/* Instead you need to pass the newley created 'playlistTracks' from your 
 						state to the Playlist component */}
-						<Playlist playlistTracks={this.state.playlistTracks} />
+						<Playlist 
+						playlistName={this.state.playlistName}
+						playlistTracks={this.state.playlistTracks}
+						onRemove={this.removeTrack} 
+						onNameChange={this.updatePlaylistName} 
+						onSave={this.savePlaylist} />
 					</div>
 				</div>
 			</div>
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -11,17 +11,26 @@ export class Track extends React.Component {
 		// anywhere so this bit of code is going to cause erros beacuse untill you
 		// create the functions and pass them down to this component
 
-		// this.addTrack = this.addTrack.bind(this);
-		// this.removeTrack = this.removeTrack.bind(this);
+		this.addTrack = this.addTrack.bind(this);
+		this.removeTrack = this.removeTrack.bind(this);
 	}
 
 	renderAction() {
 		if (this.props.isRemoval) {
-			return <button className="Track-action">-</button>;
-		} else {
-			return <button className="Track-action">+</button>;
+		  return (<button className="Track-action" onClick={this.removeTrack}> - </button>);
 		}
-	}
+		else {
+		  return (<button className="Track-action" onClick={this.addTrack}> + </button>);
+		}
+	  }
+
+	addTrack() {
+		this.props.onAdd(this.props.track);
+	  }
+
+	  removeTrack() {
+		this.props.onRemove(this.props.track);
+	  }
 
 	render() {
 		return (
