ede81b06b7aa65acf45e053fbfb52d6f876dc4c2
Henry Barnett <henry@ascender.co.uk>
2023-01-11 20:17:52 +0000

    Add comments to Spotify.js and SearchBar.js to aid issues in project

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,31 +1,39 @@
 import React from "react";
 
-import './SearchBar.css';
+import "./SearchBar.css";
 
 class SearchBar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { term: '' };
-        this.search = this.search.bind(this);
-        this.handleTermChange = this.handleTermChange.bind(this);
-    }
+	constructor(props) {
+		super(props);
+		this.state = { term: "" };
+		this.search = this.search.bind(this);
+		this.handleTermChange = this.handleTermChange.bind(this);
+	}
 
-    search() {
-        this.props.onSearch(this.state.term);
-    }
+	search() {
+		this.props.onSearch(this.state.term);
+	}
 
-    handleTermChange(event) {
-        this.setState({term: event.target.value});
-    }
+	handleTermChange(event) {
+		this.setState({ term: event.target.value });
+	}
 
-    render() {
-        return(
-            <div className="SearchBar">
-        <input  onChange={this.handleTermChange} placeholder="Enter A Song, Album, or Artist" />
-        <button className="SearchButton">SEARCH</button>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className="SearchBar">
+				<input
+					onChange={this.handleTermChange}
+					placeholder="Enter A Song, Album, or Artist"
+				/>
+
+				{/* The button needs the search function passed to the onClick property for it to work */}
+				{/* <button className="SearchButton">SEARCH</button> */}
+				<button onClick={this.search} className="SearchButton">
+					SEARCH
+				</button>
+			</div>
+		);
+	}
 }
 
 export default SearchBar;
